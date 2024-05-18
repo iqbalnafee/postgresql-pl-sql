2a94e001577511151d45c7164a1b7cc743219a5e
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2024-05-18 14:38:19 +0200

    looping done with label

--- pl-sql-block.sql
+++ pl-sql-block.sql
@@ -5,11 +5,11 @@ $$
         counter int := 0;
     begin
 
-
+        <<my_loop>>
         loop
             counter = counter + 1;
             raise notice '%', counter;
-            exit when counter = 5;
+            exit my_loop when counter = 5;
         end loop;
 
     end;
