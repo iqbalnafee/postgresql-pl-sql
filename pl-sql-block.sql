ec7fdbd29e253574778491d193a423142db58996
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2024-05-18 14:47:20 +0200

    while loop done

--- pl-sql-block.sql
+++ pl-sql-block.sql
@@ -2,25 +2,13 @@ do
 $$
 
     declare
-        row_var int := 0;
-        col_var int := 0;
+        counter int := 0;
     begin
 
-        <<outer_loop>>
-        loop
-            row_var = row_var + 1;
-
-            <<inner_loop>>
-            loop
-                col_var = col_var + 1;
-                raise notice '(%, %)', row_var,col_var;
-                exit inner_loop when col_var = 3;
-            end loop;
-            col_var := 0;
-
-
-            exit outer_loop when row_var = 3;
-        end loop;
+       while counter < 5 loop
+           raise notice 'Counter %', counter;
+		   counter := counter + 1;
+       end loop;
 
     end;
 
