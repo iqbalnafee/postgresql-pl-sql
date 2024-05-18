4d76b97d3ee10797f2b2ee2c54b702057c855025
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2024-05-18 12:12:42 +0200

    record with for loop done

--- pl-sql-block.sql
+++ pl-sql-block.sql
@@ -4,12 +4,18 @@ $$
     declare
 
         rec record;
+        counter int := 0;
 
     begin
 
-        select * into rec from email.person where id = 'allen-p';
+        for rec in select * from email.person where works_in = 'DEF'
+        loop
+            counter = counter+1;
+            raise notice '% first name is %, email address is %, works in %s',
+                counter,rec.firstname,rec.email_address, rec.works_in;
+        end loop;
 
-        raise notice 'first name is %, email address is %',rec.firstname,rec.email_address;
+
 
     end;
 $$
