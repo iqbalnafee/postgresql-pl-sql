144bb7a0afed48c006acec866cf9c2b4e79575b5
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2024-05-18 14:37:49 +0200

    looping done

--- pl-sql-block.sql
+++ pl-sql-block.sql
@@ -2,20 +2,16 @@ do
 $$
 
     declare
-
-        rec record;
         counter int := 0;
-
     begin
 
-        for rec in select * from email.person where works_in = 'DEF'
+
         loop
-            counter = counter+1;
-            raise exception 'duplicate email: %', rec.email_address
-		        using hint = 'check the email again';
+            counter = counter + 1;
+            raise notice '%', counter;
+            exit when counter = 5;
         end loop;
 
-
-
     end;
-$$
+
+$$;
