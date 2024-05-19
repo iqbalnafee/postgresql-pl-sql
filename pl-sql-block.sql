94724f06d6778611cf976002bc46636bdcd0d63c
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2024-05-19 16:11:51 +0200

    assignment task: Words and number of occurrences for all email done

--- pl-sql-block.sql
+++ pl-sql-block.sql
@@ -1,24 +1,22 @@
-create or replace function
-    word_and_number_of_occurrences(email_text email.emails.message_body%type)
-    returns table
+create or replace function word_and_number_of_occurrences_in_all_mail()
+    returns TABLE
             (
                 word       varchar,
-                occurrence int
+                occurrence integer
             )
     language plpgsql
 as
 $$
 
 declare
-    elements text[];
     var_r    record;
 begin
-    elements := string_to_array(trim(email_text), ' ');
-    for var_r in (select element as element, count(*) as occurrence
-                  from unnest(elements) element
+    for var_r in (select elements as element, count(*) as occurrence
+                  FROM (select regexp_split_to_table(emails.message_body, '\s+') AS element
+                        FROM email.emails) AS elements
                   where element <> ''
-                  group by element
-                  order by element, occurrence)
+                  group by elements
+                  order by elements)
         loop
             word := var_r.element;
             occurrence := var_r.occurrence;
@@ -27,3 +25,4 @@ begin
 
 end;
 $$;
+
